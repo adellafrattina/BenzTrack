fc0a1bb71dfd7310544fa773bfbac983b62ce317
adellafrattina <adellafrattina@studenti.uninsubria.it>
2025-05-12 15:04:45 +0200

    Added javadoc comments

--- app/src/main/java/it/uninsubria/benztrack/Exceptions.kt
+++ app/src/main/java/it/uninsubria/benztrack/Exceptions.kt
@@ -1,34 +1,114 @@
 package it.uninsubria.benztrack
 
+/**
+ * When the login procedure fails
+ *
+ * @author adellafrattina
+ */
 public class LoginException(
 
+    /**
+     * Generic message
+     */
     message: String
 ) : Exception(message)
 
+/**
+ * When the registration procedure fails
+ *
+ * @author adellafrattina
+ */
 public class RegistrationException(
 
+    /**
+     * Generic message
+     */
     message: String,
+
+    /**
+     * Error with the username. If empty, then there are no errors
+     */
     val username: String,
+
+    /**
+     * Error with the password. If empty, then there are no errors
+     */
     val password: String,
+
+    /**
+     * Error with the email. If empty, then there are no errors
+     */
     val email: String,
+
+    /**
+     * Error with the name. If empty, then there are no errors
+     */
     val name: String,
+
+    /**
+     * Error with the surname. If empty, then there are no errors
+     */
     val surname: String
 ) : Exception(message) {
 
     constructor(message: String): this(message, "", "", "", "", "")
 }
 
+/**
+ * When the car model creation procedure fails
+ *
+ * @author adellafrattina
+ */
 public class CarModelException(
 
+    /**
+     * Generic message
+     */
     message: String,
+
+    /**
+     * Error with the name. If empty, then there are no errors
+     */
     val name: String,
+
+    /**
+     * Error with the year. If empty, then there are no errors
+     */
     val year: String,
+
+    /**
+     * Error with the capacity. If empty, then there are no errors
+     */
     val capacity: String,
+
+    /**
+     * Error with the fuel type. If empty, then there are no errors
+     */
     val fuel: String,
+
+    /**
+     * Error with the CO2 factor. If empty, then there are no errors
+     */
     val co2factor: String,
+
+    /**
+     * Error with the weight. If empty, then there are no errors
+     */
     val weight: String,
+
+    /**
+     * Error with the width. If empty, then there are no errors
+     */
     val width: String,
+
+    /**
+     * Error with the length. If empty, then there are no errors
+     */
     val length: String,
+
+    /**
+     * Error with the height. If empty, then there are no errors
+     */
     val height: String
 ) : Exception(message) {
 
--- app/src/main/java/it/uninsubria/benztrack/Resources.kt
+++ app/src/main/java/it/uninsubria/benztrack/Resources.kt
@@ -5,10 +5,22 @@ import android.widget.Toast
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentReference
 
+/**
+ * Toast wrapper class to avoid message queuing
+ *
+ * @author Le1nism
+ */
 object ToastManager {
 
     private var currentToast: Toast? = null
 
+    /**
+     * Show the toast
+     *
+     * @param context The activity context
+     * @param message The message to display
+     * @param duration The duration. Possible values: Toast.LENGTH_SHORT or Toast.LENGTH_LONG
+     */
     fun show(context: Context, message: String?, duration: Int) {
 
         currentToast?.cancel()
@@ -16,36 +28,118 @@ object ToastManager {
     }
 }
 
+/**
+ * User data wrapper
+ *
+ * @author adellafrattina
+ */
 public data class User(
 
+    /**
+     * The user id
+     */
     var username: String,
+
+    /**
+     * The user password
+     */
     var password: String,
+
+    /**
+     * The user email
+     */
     var email: String,
+
+    /**
+     * The user name
+     */
     var name: String,
+
+    /**
+     * The user surname
+     */
     var surname: String
 ) {
 
     constructor(): this("", "", "", "", "")
 }
 
+/**
+ * All supported fuel types
+ *
+ * @author adellafrattina
+ */
 enum class FuelType {
 
+    /**
+     * For cars fueled by petrol
+     */
     Petrol,
+
+    /**
+     * For cars fueled by diesel
+     */
     Diesel,
+
+    /**
+     * For electric cars
+     */
     Electric
 }
 
+/**
+ * Car model data wrapper
+ */
 public data class CarModel(
 
+    /**
+     * The model's name (e.g. Alfa Romeo Tonale)
+     */
     var name: String,
+
+    /**
+     * The model's year (needs to be valid)
+     */
     var year: Int,
+
+    /**
+     * The model's capacity (expressed in cm^3)
+     */
     var capacity: Int,
+
+    /**
+     * The model's fuel type
+     */
     var fuel: FuelType,
+
+    /**
+     * The model's CO2 factor (in grams/kilometers)
+     */
     var co2factor: Float,
+
+    /**
+     * The model's weight (in kilograms)
+     */
     var weight: Float,
+
+    /**
+     * The model's width (in meters)
+     */
     var width: Float,
+
+    /**
+     * The model's length (in meters)
+     */
     var length: Float,
+
+    /**
+     * The model's height (in meters)
+     */
     var height: Float,
+
+    /**
+     * The possible search terms that will be used in the search model algorithm
+     */
     var searchterms: ArrayList<String>
 ) {
 
@@ -58,25 +152,75 @@ public data class CarModel(
     }
 }
 
+/**
+ * Car data wrapper
+ *
+ * @author adellafrattina
+ */
 public data class Car(
 
+    /**
+     * The car's plate string
+     */
     var plate: String,
+
+    /**
+     * The car's arbitrary name
+     */
     var name: String,
+
+    /**
+     * The car's model
+     */
     var model: DocumentReference?,
+
+    /**
+     * The next maintenance date
+     */
     var maintenancedate: Timestamp?,
+
+    /**
+     * The next insurance date
+     */
     var insurancedate: Timestamp?,
+
+    /**
+     * The next tax date
+     */
     var taxdate: Timestamp?
 ) {
 
     constructor(): this("", "", null, null, null, null)
 }
 
+/**
+ * Refill data wrapper
+ */
 public data class Refill(
 
+    /**
+     * The refill's date
+     */
     var date: Timestamp?,
+
+    /**
+     * The refill's fuel dispenser position (should be expressed in coordinates)
+     */
     var position: String,
+
+    /**
+     * The refill's price per liter (euros/liters)
+     */
     var ppl: Float,
+
+    /**
+     * The car's mileage at the time of the refill. The name is misleading, since it is measured in kilometers and not miles
+     */
     var mileage: Float,
+
+    /**
+     * The refill's amount in euros
+     */
     var amount: Float
 ) {
 
