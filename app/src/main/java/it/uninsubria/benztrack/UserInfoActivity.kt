2a01dcb32112cf3bc8d2665d80731377d97311a3
le1nism <cristian.capiferri@gmail.com>
2025-05-19 18:35:45 +0200

    Removed hardcoded strings and cleaned up code

--- app/src/main/java/it/uninsubria/benztrack/UserInfoActivity.kt
+++ app/src/main/java/it/uninsubria/benztrack/UserInfoActivity.kt
@@ -2,12 +2,10 @@ package it.uninsubria.benztrack
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 
 
 class UserInfoActivity : AppCompatActivity() {
