b837f293774e56dcfdb6ef8a133a1c4472627012
le1nism <cristian.capiferri@gmail.com>
2025-06-21 17:23:03 +0200

    Code cleanup

--- app/src/main/java/it/uninsubria/benztrack/AddCarActivity.kt
+++ app/src/main/java/it/uninsubria/benztrack/AddCarActivity.kt
@@ -2,11 +2,8 @@ package it.uninsubria.benztrack
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
-import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
