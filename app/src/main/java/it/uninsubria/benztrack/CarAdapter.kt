940e4706fc1ba6629845e75f1a2e768ea62c9350
le1nism <cristian.capiferri@gmail.com>
2025-06-22 21:30:08 +0200

    Implemented clickable card for user's cars

--- app/src/main/java/it/uninsubria/benztrack/CarAdapter.kt
+++ app/src/main/java/it/uninsubria/benztrack/CarAdapter.kt
@@ -6,7 +6,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class CarAdapter(private val cars: List<Car>) : RecyclerView.Adapter<CarAdapter.CarViewHolder>() {
+class CarAdapter(private val cars: List<Car>, private val onCarClick: (Car) -> Unit) : RecyclerView.Adapter<CarAdapter.CarViewHolder>() {
     class CarViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val name: TextView = itemView.findViewById(R.id.text_car_name)
         val model: TextView = itemView.findViewById(R.id.text_car_model)
@@ -31,6 +31,10 @@ class CarAdapter(private val cars: List<Car>) : RecyclerView.Adapter<CarAdapter.
             }
         }?.addOnFailureListener {
             holder.model.text = "Error loading model"
+        }
+
+        holder.itemView.setOnClickListener {
+            onCarClick(car)
         }
     }
 
