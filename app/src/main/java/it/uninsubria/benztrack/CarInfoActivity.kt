56f16f5ff98ed3e0252a61682a436259d0c360fd
le1nism <cristian.capiferri@gmail.com>
2025-06-24 22:43:47 +0200

    Removed redundant code

--- app/src/main/java/it/uninsubria/benztrack/CarInfoActivity.kt
+++ app/src/main/java/it/uninsubria/benztrack/CarInfoActivity.kt
@@ -158,9 +158,6 @@ class CarInfoActivity : AppCompatActivity() {
                     .addOnFailureListener { e ->
                         ToastManager.show(this, e.message, Toast.LENGTH_SHORT)
                     }
-                ToastManager.show(this, "Insurance date set to $formatted", Toast.LENGTH_SHORT)
-                payInsuranceButton.isEnabled = true
-                payInsuranceButton.alpha = 1.0f
             }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH))
             dialog.datePicker.minDate = System.currentTimeMillis()
             dialog.show()
